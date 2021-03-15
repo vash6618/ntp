149ce9a547942a8c647790dadd2e28b33daef936
Vasu Sharma <vasusharma@Vasus-MacBook-Pro.local>
2021-03-15 11:18:29 -0600

    changed the bind port of the server

--- ntp_server.py
+++ ntp_server.py
@@ -84,7 +84,7 @@ class NTPPacket:
 def print_time(t):
     print(t, "{0:.16f}".format(t))
 
-UDP_IP = "127.0.0.1"
+UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, # Internet
